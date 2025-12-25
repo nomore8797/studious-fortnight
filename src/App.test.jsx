55f9ec523b8be0a390af1de35ac620fb89f2c7d2
nomore8797 <nomore8797@gmail.com>
2025-12-25 16:44:09 +0300

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,6 @@
-// src/App.test.jsx
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders main header', () => {
   render(<App />);
   const headerElement = screen.getByText(/GitHub Codespaces/i);
