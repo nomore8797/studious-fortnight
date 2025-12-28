983d278854076efe27659fcddc56bb62abbec8f2
nomore8797 <nomore8797@gmail.com>
2025-12-28 16:52:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-test: {
-  globals: true,
-  setupFiles: './vitest.setup.js',
-  environment: 'jsdom',
-},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,                 // чтобы можно было использовать глобальные функции Vitest без импорта
+    environment: 'jsdom',          // среда тестирования, нужна для React
+    setupFiles: './vitest.setup.js', // файл для предварительной настройки тестов
+    include: ['src/**/*.test.jsx'], // какие файлы тестов запускать
+    coverage: {                     // опционально: отчёт по покрытию
+      reporter: ['text', 'lcov']
+    }
+  }
+})
