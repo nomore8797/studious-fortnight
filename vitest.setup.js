cb553ad5d6ca098a5ad44bf9bb839e5074a35889
nomore8797 <nomore8797@gmail.com>
2025-12-28 16:55:35 +0300

    Update vitest.setup.js

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,1 +1,14 @@
-import '@testing-library/jest-dom';
+// vitest.setup.js
+import '@testing-library/jest-dom'  // добавляет матчеры типа toBeInTheDocument, toBeEnabled и т.д.
+import { cleanup } from '@testing-library/react'
+import { afterEach } from 'vitest'
+
+// Очистка после каждого теста, чтобы компоненты не накапливались
+afterEach(() => {
+  cleanup()
+})
+
+// Если где-то используется ReactDOMTestUtils.act, перенаправим на React.act
+import React from 'react'
+import { act as reactAct } from 'react-dom/test-utils'
+global.act = reactAct
