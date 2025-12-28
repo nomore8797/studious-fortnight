4d13c90c5fb15129e87bd73fdbc973b394269c32
nomore8797 <nomore8797@gmail.com>
2025-12-28 16:31:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
-test: {
-  globals: true,
-  setupFiles: './vitest.setup.js',
-  environment: 'jsdom',
-},
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.js'
+  }
+})
